6b99ec5b267f6becabf21a8aefb1ef529ae41728
yuchan <yusuke@junkpiano.me>
2014-08-25 10:14:44 +0900

    changed config about assets compile.

--- config/application.rb
+++ config/application.rb
@@ -7,17 +7,29 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module FirstApp
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    class Application < Rails::Application
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration should go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+        # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+        # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+        # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-  end
+        # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+        # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+        # config.i18n.default_locale = :de
+
+        # We don't want the default of everything that isn't js or css, because it pulls too many things in
+        config.assets.precompile.shift
+        config.assets.precompile.push(Proc.new do |path|
+            File.extname(path).in? [
+                '.html', '.erb', '.haml',                 # Templates
+                '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+                '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+            ]
+        end)
+        # config/environments/production.rb
+        config.assets.compile = true
+    end
 end
